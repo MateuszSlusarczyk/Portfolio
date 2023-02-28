490984d7b8415198b8f61ee72bcbc6ae1f39dc1f
MateuszSlusarczyk <126509313+MateuszSlusarczyk@users.noreply.github.com>
2023-02-28 13:59:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-var buttons = document.querySelectorAll(".Nav");
-var sections = document.querySelectorAll(".view");
-
-buttons[0].addEventListener("click", function(){
-    sections[0].classList.add('active');
-    window.location.href='#Omnie';
-})
-buttons[1].addEventListener("click", function(){
-    sections[1].classList.add('active');
-    window.location.href='#Omnie';
-})
-buttons[2].addEventListener("click", function(){
-    sections[2].classList.add('active');
-    window.location.href='#Omnie';
-})
-buttons[3].addEventListener("click", function(){
-    sections[3].classList.add('active');
-})
-
 $(document).ready(function(){
     var scrollLink = $('.Nav');
 
@@ -26,16 +7,4 @@ $(document).ready(function(){
             scrollTop: $(this.hash).offset().top
         }, 700)
     })
-
-
-$(window).scroll(function(){
-    var scrollBarLocation = $(this).scrollTop();
-    scrollLink.each(function(){
-        var sectionOffset = $(this.hash).offset().top-20;
-        if(sectionOffset <= scrollBarLocation){
-            $(this).addClass('Current');
-            $(this).siblings().removeClass('Current');
-        }
-    })
-})
 })
