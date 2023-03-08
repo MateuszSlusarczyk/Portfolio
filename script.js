49ca91fda2d0b7ae1af7b333cfe9639f2e90206d
MateuszSlusarczyk <126509313+MateuszSlusarczyk@users.noreply.github.com>
2023-03-08 12:21:01 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 $(document).ready(function(){
     var scrollLink = $('.Nav');
-
+    var scrollChange = $('.item');
     scrollLink.click(function(e){
         e.preventDefault();
         $('body,html').animate({
             scrollTop: $(this.hash).offset().top
         }, 700)
+    })
+    $(window).scroll(function(){
+        var scrollBarLocation = $(this).scrollTop();
+        scrollChange.each(function(){
+            var sectionOffset = $(this.hash).offset().top-20;
+            if(sectionOffset <= scrollBarLocation){
+                $(this).addClass('Current');
+                $(this).siblings().removeClass('Current');
+            }
+        })
     })
 })
